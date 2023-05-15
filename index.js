22af79f8a0a66a1eb36153cb36022533b341a925
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-05-15 10:09:45 +0800

    cors options

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const authRoutes = require('./routes/auth');
 require('dotenv').config();
 app.use(express.json());
 
+const corsOptions = {
+  origin: ['http://localhost:3000/', 'https://grad-app-frontend.vercel.app/'],
+  optionsSuccessStatus: 200
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 mongoose.connect('mongodb+srv://jhonchristianubaldo:christiankyzen@explore-ro8.qqees76.mongodb.net/ro8-explore?retryWrites=true&w=majority', {
   useNewUrlParser: true,
