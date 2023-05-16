b1ae0a1ed5ca6f814b8b6cefecc49bc74e39d144
Jhay1413 <jhonchristian.ubaldo@evsu.edu.ph>
2023-05-16 15:24:22 +0800

    fixong cors options

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 app.use(express.json());
 
 const corsOptions = {
-  origin: ['https://grader-app-frontend.vercel.app','http://localhost:3000'],
+  origin: ['https://grad-app-frontend.vercel.app','http://localhost:3000'],
   optionsSuccessStatus: 200
 };
 
